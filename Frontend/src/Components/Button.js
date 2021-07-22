86bbe95ebd013671888af77c93f7c1a41f2c51c6
sadra <sadrataghavi1383@gmail.com>
2021-07-22 16:38:22 +0430

    removed one of the btns

--- Frontend/src/Components/Button.js
+++ Frontend/src/Components/Button.js
@@ -8,6 +8,7 @@ const STYLES = ['btn--primary', 'btn--outline', 'btn--test'];
 const SIZES = ['btn--medium', 'btn--large'];
 
 export const Button = ({
+  link,
   children,
   type,
   onClick,
@@ -21,7 +22,7 @@ export const Button = ({
   const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
 
   return (
-    <Link to='/sign-up' className='btn-mobile'>
+    <Link to={link} className='btn-mobile'>
       <button
         className={`btn ${checkButtonStyle} ${checkButtonSize}`}
         onClick={onClick}
