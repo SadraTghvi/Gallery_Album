72b1fe9155dda9b1c0bc0b9f117282be2b60ffc3
sadra <sadrataghavi1383@gmail.com>
2021-07-22 18:22:06 +0430

    h1 tag added

--- Frontend/src/Components/CardsHolder.js
+++ Frontend/src/Components/CardsHolder.js
@@ -6,6 +6,9 @@ import ImgCard from './ImgCard'
 function CardsHolder() {
     return (
         <>
+            <div>
+                <h1 id="gallery">Gallery Section</h1>
+            </div>
             <div className="parent">
                 <div className="CardsHolder">
                     <ImgCard src="static/img/img-1.jpg"/>
