9cdb847a1d1daa98e54b24c81d3895bc24b8e4e4
sadra <sadrataghavi1383@gmail.com>
2021-07-23 23:13:53 +0430

    I AM SO FKING GENUIS :)

--- Frontend/src/Components/CardsHolder.js
+++ Frontend/src/Components/CardsHolder.js
@@ -1,8 +1,33 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
+import axios from 'axios';
 import './CardsHolder.css';
 import ImgCard from './ImgCard';
 
 function CardsHolder() {
+  const [data,setData] = useState([])
+
+  const getPics = () =>{
+    axios.get('http://127.0.0.1:8000/api/')
+    .then(res => {
+      setData(res.data)
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  }
+  
+  useEffect(() => {
+    getPics();
+  }, [])
+
+  // useEffect(() => {
+  //   console.log(finalData)
+  // }, [finalData])
+  
+  // for (let i = 0; i < data.length; i++) {
+  //   console.log(data[i])
+  // }
+
   return (
     <div className='cards'>
       <h1>Gallery Album</h1>
@@ -10,43 +35,33 @@ function CardsHolder() {
         <div className='cards__wrapper'>
           <ul className='cards__items'>
             <ImgCard
-              src='static/img/img-1.jpg'
-              text='Explore the hidden waterfall deep inside the Amazon Jungle'
-              label='Adventure'
-              path='/services'
-            />
-            <ImgCard
               src='static/img/img-2.jpg'
-              text='Travel through the Islands of Bali in a Private Cruise'
-              label='Luxury'
-              path='/services'
-            />
-          </ul>
-          <ul className='cards__items'>
-            <ImgCard
-              src='static/img/img-3.jpg'
               text='Set Sail in the Atlantic Ocean visiting Uncharted Waters'
               label='Mystery'
               path='/services'
             />
+
             <ImgCard
-              src='static/img/img-4.jpg'
-              text='Experience Football on Top of the Himilayan Mountains'
-              label='Adventure'
-              path='/products'
-            />
-            <ImgCard
-              src='static/img/img-5.jpg'
-              text='Ride through the Sahara Desert on a guided camel tour'
-              label='Adrenaline'
-              path='/sign-up'
-            />
-            <ImgCard
-              src='static/img/img-6.jpg'
-              text='Ride through the Sahara Desert on a guided camel tour'
+              src='static/img/img-1.jpg'
+              text='Set Sail in the Atlantic Ocean visiting Uncharted Waters'
               label='Mystery'
-              path='/sign-up'
+              path='/services'
             />
+          
+                      
+          </ul>
+          <ul className='cards__items mine'>
+          
+          {data.map((item) =>
+            <ImgCard
+              key={item.id}
+              src={"static"+item.img}
+              text={item.description}
+              label={item.Title}
+              path='/services'
+            />
+          )}
+             
           </ul>
         </div>
       </div>
