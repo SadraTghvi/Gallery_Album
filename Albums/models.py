63fa59a89719913d9fc73d531a9ba327af2f3874
sadra <sadrataghavi1383@gmail.com>
2021-07-23 23:13:14 +0430

    models edited title => Title

--- Albums/models.py
+++ Albums/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Albums(models.Model):
+    Title = models.CharField(max_length=100)
+    img = models.ImageField(blank=False)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.Title
