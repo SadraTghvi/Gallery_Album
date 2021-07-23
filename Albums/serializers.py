a27819412d3c2ad0ea460fed75485194487736ec
sadra <sadrataghavi1383@gmail.com>
2021-07-23 19:50:21 +0430

    serializer for api

---
+++ Albums/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from Albums.models import *
+
+
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Albums
+        fields = "__all__"
