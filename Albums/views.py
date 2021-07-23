67217c8f5c8c582ebbb6869a6ce53adfed1f77a2
sadra <sadrataghavi1383@gmail.com>
2021-07-23 19:51:13 +0430

    sending obj to rest

--- Albums/views.py
+++ Albums/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect, JsonResponse
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from Albums.serializers import *
+
+from Albums.models import *
+
 
 # Create your views here.
+@api_view(["GET"])
+def getPics(request):
+    pics = Albums.objects.all()
+    serializer = TaskSerializer(pics,many=True)
+    return Response(serializer.data)
