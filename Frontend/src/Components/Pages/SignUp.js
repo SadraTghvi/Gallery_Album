96010ab26ea4bf180482e884e6663c7e909e0179
sadra <sadrataghavi1383@gmail.com>
2021-07-24 22:50:49 +0430

    making a post req to backend

--- Frontend/src/Components/Pages/SignUp.js
+++ Frontend/src/Components/Pages/SignUp.js
@@ -1,37 +1,51 @@
 import React,{useState,useEffect} from 'react'
 import axios from 'axios'
 import "./SignUp.css"
+import Cookies from 'js-cookie'
+
+const config = {
+        headers: {
+            'Content-Type': 'application/json',
+            'X-CSRFToken': document.currentScript.getAttribute('csrfToken') 
+            },
+    };
 
 function SignUp() {
 
     const [userPass,setUserPass] = useState({username:"",password:""})
 
     useEffect(() => {
-        console.log(userPass)
+        console.log(userPass)    
     }, [userPass])
 
-    // const sendForm = () =>{
-    //     axios.post()
-    // }
+    
+
+    const sendForm = () =>{
+        axios.post('http://127.0.0.1:8000/api/signup/',{
+            username:userPass.username,
+            password:userPass.password
+        },config)
+        
+    
+    }
 
     const formHandler = (e) =>{
         e.preventDefault()
-        let user= document.getElementById("user").value
-        let pass= document.getElementById("pass").value
         setUserPass({
             username:user,
             password:pass,
         })
+        sendForm();
     }
 
     let formInputs = <div>
-        <div class="input-group mb-3">
+        <div className="input-group mb-3">
             <span className="input-group-text" id="basic-addon1">User</span>
-            <input type="text" id="user" name="user" className="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1" />
+            <input type="text" id="user" onChange={e => setUserPass({...userPass,username:e.target.value})} name="user" className="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1" />
         </div>
-        <div class="input-group mb-3">
+        <div className="input-group mb-3">
             <span className="input-group-text" id="basic-addon1">Pass</span>
-            <input type="password" id="pass" name="pass" className="form-control" placeholder="Password" aria-label="Username" aria-describedby="basic-addon1" />
+            <input type="password" id="pass" onChange={e => setUserPass({...userPass,password:e.target.value})} name="pass" className="form-control" placeholder="Password" aria-label="Username" aria-describedby="basic-addon1" />
         </div>
     </div>
 
