be5f166db6d9afc08724736109bd9d95c0434fe8
sadra <sadrataghavi1383@gmail.com>
2021-07-26 14:12:41 +0430

    styling for card

---
+++ Frontend/src/Components/Info.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import "./Info.css"
+
+const COLOR = ["black","red","green"]
+
+
+function Info(props) {
+    const CheckColor = COLOR.includes(props.color) ? props.color : COLOR[0]
+
+    return (
+        <>
+            <div className={`info_main ${CheckColor} ` }>
+                <div className="text">{props.text}</div>
+            </div>
+        </>
+    )
+}
+
+Info.defaultProps = {
+    text: "",
+}
+
+export default Info
