4c47b4dac1f9ecc0b915e7f1dca31696c69a34e3
sadra <sadrataghavi1383@gmail.com>
2021-07-27 01:09:16 +0430

    account url edited

--- Account/urls.py
+++ Account/urls.py
@@ -4,6 +4,7 @@ from Account import views
 urlpatterns = [
     path("",views.account,name="account"),
     path("signup/", views.signUp, name="signUp"),
+    path("logout/", views.logOut, name="logout"),
 ]
 
 app_name = "account"
