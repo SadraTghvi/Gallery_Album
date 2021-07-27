f1b2a6b37aff09de01513423f2f85d1c806cd90e
sadra <sadrataghavi1383@gmail.com>
2021-07-27 11:14:32 +0430

    url for login api

--- Account/urls.py
+++ Account/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path("",views.account,name="account"),
     path("signup/", views.signUp, name="signUp"),
     path("logout/", views.logOut, name="logout"),
+    path("login/", views.logIn, name="login"),
 ]
 
 app_name = "account"
