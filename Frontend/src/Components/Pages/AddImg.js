3773853a0ad415cd86a2fb8d6463dd70f8cc6f43
sadra <sadrataghavi1383@gmail.com>
2021-07-28 14:18:52 +0430

    worked on addImg

--- Frontend/src/Components/Pages/AddImg.js
+++ Frontend/src/Components/Pages/AddImg.js
@@ -1,7 +1,16 @@
 import React,{useEffect, useState} from 'react'
 import axios, { post } from 'axios'
+import "./AddImg.css"
 
 const config = {
+    headers:{
+        'content-type': 'multipart/form-data',
+        'Content-Type': 'application/json',
+        'X-CSRFToken': document.currentScript.getAttribute('csrfToken'),
+    }
+} 
+
+const config2 = {
     headers:{
         'content-type': 'multipart/form-data',
         'Content-Type': 'application/json',
@@ -14,22 +23,36 @@ function AddImg() {
     const [data, setdata] = useState({
         title: "",
         discription: "",
-        file: null
+        "id": null
     })
-    
-    const onFormSubmit = (e) =>{
+
+    useEffect(() => {
+        console.log(data)
+    }, [data])
+
+    const sendText = (e) =>{
         e.preventDefault()
-        fileUpload().then(res =>{
+        axios.post("addImg/getText/",data,config2)
+        .then(res =>{
             console.log(res)
         })
     }
+
+    const sendImg = () =>{
+        fileUpload().then(res =>{
+            console.log(res)
+            setdata({...data,id:res.data.albumId})
+        })
+    }
+    
+    
 
     const onChange = (e) =>{
         setfile(e.target.files[0])
     }
 
     const fileUpload = () =>{
-        const url = "addImg/add/"
+        const url = "addImg/getImg/"
         const formData = new FormData();
         formData.append("file",file)
 
@@ -44,20 +67,19 @@ function AddImg() {
 
     return (
         <>
-            <div className="main">
-                <form action="" onSubmit={onFormSubmit}>
-                    <h1 className="text">File Upload</h1>
+                    {/* <h1 >File Upload</h1> */}
+            <div className="addImg">
                     <div className="input-group mb-3">
                         <span className="input-group-text" id="basic-addon1">Title</span>
-                        <input type="text" id="user" onChange={e => settext({...data,title:e.target.value})} name="user" className="form-control" placeholder="..." aria-label="Username" aria-describedby="basic-addon1" />
+                        <input type="text"  onChange={e => setdata({...data,title:e.target.value})} name="user" className="form-control" placeholder="..." aria-label="Username" aria-describedby="basic-addon1" />
                     </div>
                     <div className="input-group mb-3">
                         <span className="input-group-text" id="basic-addon1">Discription</span>
-                        <input type="text" id="user" onChange={e => settext({...data,discription:e.target.value})} name="user" className="form-control" placeholder="..." aria-label="Username" aria-describedby="basic-addon1" />
+                        <input type="text"  onChange={e => setdata({...data,discription:e.target.value})} name="user" className="form-control" placeholder="..." aria-label="Username" aria-describedby="basic-addon1" />
                     </div>
-                    <input  type="file" onChange={onChange} />
-                    <button className="btn btn--primary" type="submit">Upload</button>
-                </form>
+                    <input required  type="file" onChange={onChange} />
+                    <button className="btn btn-danger" type="submit" onClick={sendImg}>Upload Img</button>
+                    <button className="btn btn-dark" type="submit" onClick={sendText}>Set Img text</button>
             </div>
         </>
     )
