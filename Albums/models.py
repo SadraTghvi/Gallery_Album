6909032ddf8461eb8588c4d9449f64508268f749
sadra <sadrataghavi1383@gmail.com>
2021-07-28 14:18:30 +0430

    debugge

--- Albums/models.py
+++ Albums/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Albums(models.Model):
-    Title = models.CharField(max_length=100)
-    img = models.ImageField(blank=False)
-    description = models.TextField(blank=True)
+    Title = models.CharField(max_length=100,null=True,blank=True)
+    img = models.ImageField(null=True,blank=True)
+    description = models.TextField(null=True, blank=True)
 
     def __str__(self):
-        return self.Title
+        return self.Title or "No Title"
