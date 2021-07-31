702648f8abaac48d0b9351c20f8b8dd7bd237f15
sadra <sadrataghavi1383@gmail.com>
2021-08-01 00:04:31 +0430

    getting user is_authenticated from main thread and passing them to sub apps

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,4 +1,5 @@
-import React,{useState} from 'react';
+import React,{useState,useEffect} from 'react';
+import axios from 'axios';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
@@ -15,21 +16,47 @@ import "./App.css";
 
 
 function App() {
-    const [Loaded, setLoaded] = useState(null)
+    // const [Loaded, setLoaded] = useState(null)
+    const [redirect, setredirect] = useState({
+        redirect: false,
+        username : ""
+    })
+
+    const getInfo = () =>{
+        axios.get("/manageAccount/")
+        .then(res =>{
+            console.log(res)
+            if(res.data.status === "failed"){
+                setredirect({
+                    redirect : true,
+                    username: res.data.username
+                })
+            } else{
+                setredirect({
+                    redirect : false,
+                    username: res.data.username
+                })
+            }
+        })
+    }
+
+    useEffect(() => {
+        getInfo()
+    }, [])
     
     return (
         <Router>
-            <Navbar  />
+            <Navbar  redirect={redirect}/>
             <Switch>
-                <Route path="/"        exact   component={Home}/>
+                <Route path="/" exact> <Home redirect={redirect} /> </Route>
 
 
 
-                <Route path="/account" exact   component={Account}/>
-                <Route path="/signup"  exact   component={SignUp}/> 
-                <Route path="/login"   exact   component={Login}/> 
+                <Route path="/account" exact> <Account  redirect={redirect} /> </Route>
+                <Route path="/signup"  exact> <SignUp   redirect={redirect} />  </Route>
+                <Route path="/login"   exact> <Login    redirect={redirect} />  </Route>
                 
-                <Route path="/addimg"  exact   component={AddImg}/>
+                <Route path="/addimg"  exact> <AddImg   redirect={redirect} /> </Route>
 
             </Switch>
             <Footer />
