759029d50163acf2bd9932175bb04fd5a264df88
sadra <sadrataghavi1383@gmail.com>
2021-07-31 22:05:09 +0430

    functionality updated

--- AddImg/views.py
+++ AddImg/views.py
@@ -17,6 +17,9 @@ from django.views.decorators.csrf import csrf_exempt
 
 from Albums.models import *
 
+from io import BytesIO
+from django.http.multipartparser import MultiPartParser,ChunkIter,parse_header
+
 # Create your views here.
 
 
@@ -29,65 +32,71 @@ def BodyLoader(body):
 @require_POST
 @csrf_exempt
 def addImg(request):
-        file = request.FILES.get("file")
-        if isinstance(file, UploadedFile):
-            file_type = magic.from_buffer(file.read(),mime=True)
-            if not file_type in ["image/gif","image/png","image/jpeg"]:
-                return JsonResponse({"status" : "failed","text":"File Type "})
-            if file.size > 1000000:
-                return JsonResponse({"status": "failed", "text": "File Is Bigger Than 1Mg "})
+    request.POST,request._files = MultiPartParser(
+        request.META,
+        BytesIO(request.body),
+        request.upload_handlers,
+        request.encoding,
+    ).parse()
 
-            newAlbum = Albums(
-                Title = " ",
-                img = file,
-                description = " "
-            )
+    title = request.POST.get("title")
+    discription = request.POST.get("discription")
+    file = request.FILES.get("file")
 
-            newAlbum.save()
-
-            albumId = newAlbum.id
-
-
-            return JsonResponse({
-                "status" : "success",
-                "albumId" : albumId,
-                "text" : "Your Img Saved Successfully"
-            })
-        else:
-            return JsonResponse({"status": "failed", "text": "Error To Handle File Try Again Plz"})
-
-@csrf_exempt
-@require_POST
-def addText(request):
-    data = {}
-
-    if request.POST:
-        data = request.POST
-    elif request.body:
-        data = BodyLoader(request.body)
-
-
-    try:    
-        title = data.get("title")
-        discription = data.get("discription")
-        albumId = data.get("id")
-
-        newAlbum = Albums.objects.get(id=albumId)
-
-        newAlbum.Title = title
-        newAlbum.discription = discription
+    if isinstance(file,UploadedFile):
+        file_type = magic.from_buffer(file.read(),mime=True)
+        if not file_type in ["image/gif", "image/png","image/jpeg"]:
+            return JsonResponse({"status" : "failed","text" : "File Type Not Supported"})
+        if file.size > 1000000:
+            return JsonResponse({"status" : "failed","text" : "File Is Bigger Than 1Mg"})
+            
+        newAlbum = Albums(
+             Title = title,
+             img = file,
+             description = discription
+        )
 
         newAlbum.save()
 
-        return JsonResponse({
-                    "status" : "success",
-                    "text" : "Your Album Saved Successfully"
-                })
-    except Exception as expt:
-        print(expt)
-        return JsonResponse({
-            "status": "failed",
-            "text": "There was something wrong with your request plz try again later"
-        })
+        return JsonResponse({"status" : "success","text" : "Your Album Saved Successfully"})
+    else:
+        return JsonResponse({"status" : "failed","text" : "Something Went Wrong, Try Again Later Plz"})
+
+
+            
+
+# @csrf_exempt
+# @require_POST
+# def addText(request):
+    # data = {}
+
+    # if request.POST:
+    #     data = request.POST
+    # elif request.body:
+    #     data = BodyLoader(request.body)
+
+
+    # try:    
+    #     title = data.get("title")
+    #     discription = data.get("discription")
+    #     albumId = data.get("id")
+
+    #     newAlbum = Albums.objects.get(id=albumId)
+
+    #     newAlbum.Title = title
+    #     newAlbum.discription = discription
+
+    #     newAlbum.save()
+
+    #     return JsonResponse({
+    #                 "status" : "success",
+    #                 "text" : "Your Album Saved Successfully"
+    #             })
+    # except Exception as expt:
+    #     print(expt)
+    #     return JsonResponse({
+    #         "status": "failed",
+    #         "text": "There was something wrong with your request plz try again later"
+    #     })
 
 
