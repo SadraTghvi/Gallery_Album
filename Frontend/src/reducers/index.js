161ec45a2415d6358f24a4b8e13ab93ee04c6d20
sadra <sadrataghavi1383@gmail.com>
2021-08-04 21:17:20 +0430

    cleaning useless code/clean code

--- Frontend/src/reducers/index.js
+++ Frontend/src/reducers/index.js
@@ -1,11 +1,7 @@
 import { combineReducers } from "redux";
-import ifPrint from "./account";
-import loggedIn from "./isLogged";
 import getinfo from "./getUser";
 
 const rootReducer = combineReducers({
-    ifPrint:ifPrint,
-    isLogged: loggedIn,
     getinfo: getinfo
 })
 
