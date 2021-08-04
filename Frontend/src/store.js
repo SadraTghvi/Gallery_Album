79ab659a4d73cddac0ea5da8ddd974e462704dd4
sadra <sadrataghavi1383@gmail.com>
2021-08-04 18:31:58 +0430

    store config edited

--- Frontend/src/store.js
+++ Frontend/src/store.js
@@ -1,7 +1,18 @@
-import { createStore } from "redux";
+import { createStore,applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from 'redux-thunk';
+
+const initialState = {};
+
+const middleware = [thunk];
 
 import rootReducer from "./reducers";
-const store = createStore(rootReducer,
-                          window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+
+
+const store = createStore(
+  rootReducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware)),
+);
 
 export default store;        
