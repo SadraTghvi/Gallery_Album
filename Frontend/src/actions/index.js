18cb9cefec282ea40a008ed4fdf3c7edd3035e08
sadra <sadrataghavi1383@gmail.com>
2021-08-04 21:16:32 +0430

    cleaning useless code

--- Frontend/src/actions/index.js
+++ Frontend/src/actions/index.js
@@ -1,19 +1,2 @@
 
-export const print = () =>{
-    return{
-        type: "print"
-    }
-}
-export const dontprint = () =>{
-    return{
-        type: "dontprint"
-    }
-}
-
-export const switc = () =>{
-    return{
-        type: "logged"
-    }
-}
-
 
