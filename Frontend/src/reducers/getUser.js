18dde4919dff8ab74a46b5708e5af32c67915f19
sadra <sadrataghavi1383@gmail.com>
2021-08-04 18:32:13 +0430

    reducer for getting user info

---
+++ Frontend/src/reducers/getUser.js
@@ -0,0 +1,20 @@
+const initialState = {
+    username: " ",
+    isLoggedIn: false
+}
+
+const getinfo = (state = initialState, { type, payload }) => {
+    switch (type) {
+
+    case "loggedIn":
+        return { username:payload,isLoggedIn:true }
+
+    case "notLoggedIn":
+        return { ...state, username:"anonymous" }
+
+    default:
+        return state
+    }
+}
+
+export default getinfo;
