54793c1e65b99ca57970708fa7ec719390b313b8
sadra <sadrataghavi1383@gmail.com>
2021-08-05 15:46:20 +0430

    using redux and redirect when user logged in

--- Frontend/src/Components/Pages/Account.js
+++ Frontend/src/Components/Pages/Account.js
@@ -1,36 +1,43 @@
 import axios from 'axios';
 import React, { useEffect,useState } from 'react'
+
+import { useDispatch, useSelector } from 'react-redux';
+import { getUserInfo } from '../../actions/getUser';
+
 import { Link, Redirect, useHistory } from 'react-router-dom';
 
 import "./Account.css"
 
 function Account() {
     const [redirect, setredirect] = useState(false)
+    
+    
 
-    let history = useHistory();
+    const dispatch = useDispatch()
+    
+    useEffect(() => {
+        dispatch(getUserInfo())
+    }, [])
+    
+    const info = useSelector(state => state.getinfo)
+
+    useEffect(() => {
+        setredirect(!info.isLoggedIn)
+    }, [info])
+
+    useEffect(() => {
+        console.log(redirect)
+    }, [redirect])
 
 
     const logOut = () =>{
         window.location.replace("manageAccount/logout/");
     }
 
-    // const getInfo = () =>{
-    //     axios.get("/manageAccount/")
-    //     .then(res =>{
-    //         console.log(res)
-    //         if(res.data.status === "failed"){
-    //             setredirect(true)
-    //         } else{
-    //             setredirect(false)
-    //         }
-    //     })
-    // }
-
     
-    // useEffect(() => {
-    //     getInfo()
-    // }, [])
-
+    if(redirect){
+        return <Redirect to="/login" />
+    }
     
     return (
         <>
